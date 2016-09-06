751910f5e9a27ac2caca6adb38e784067663ff58
magnet <millennion@openmailbox.org>
2016-09-06 00:38:27 -0500

    Refactor

--- magnets/image_viewer.py
+++ magnets/image_viewer.py
@@ -5,6 +5,22 @@ image = pyglet.resource.image('np_panda.png')
 
 
 @window.event
+def on_key_press(symbol, modifiers):
+    if symbol == pyglet.window.key.A:
+        print 'The "A" key was pressed.'
+    elif symbol == pyglet.window.key.LEFT:
+        print 'The left arrow key was pressed.'
+    elif symbol == pyglet.window.key.RIGHT:
+        print 'The right arrow key was pressed.'
+    elif symbol == pyglet.window.key.UP:
+        print 'The up arrow key was pressed.'
+    elif symbol == pyglet.window.key.DOWN:
+        print 'The down arrow key was pressed.'
+    elif symbol == pyglet.window.key.ENTER:
+        print 'The enter key was pressed.'
+
+
+@window.event
 def on_draw():
     window.clear()
     image.blit(0, 0)
